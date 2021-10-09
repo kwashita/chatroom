e8039ffdaad9cb9951b1db1a292d836f0686275f
Heng <165647619@qq.com>
2021-10-09 15:32:10 +1100

    added upload route for statis and restframework for api

--- chatroom/chatroom/settings.py
+++ chatroom/chatroom/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp'
+    'myapp',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/upload/'
+MEDIA_ROOT = BASE_DIR / 'upload'
