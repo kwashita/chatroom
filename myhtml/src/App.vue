ab2e0ce9afc674259a395a70e17c6a2aee280410
Heng <165647619@qq.com>
2021-10-15 07:30:16 +1100

    add Vue

--- myhtml/src/App.vue
+++ myhtml/src/App.vue
@@ -1,5 +1,44 @@
 <template>
-  <div>Hello</div>
+  <div id="page-box">
+    <img src="http://127.0.0.1:9000/upload/image1.jpg" alt="" />
+    <h1>123123</h1>
+  </div>
 </template>
-<script></script>
-<style></style>
+<script>
+import axios from "axios";
+export default {
+  date() {
+    return {
+      text: "",
+    };
+  },
+
+  mounted() {
+    this.getData();
+  },
+
+  methods: {
+    //retreive data
+    getData() {
+      axios({
+        method: "get",
+        url: "http://127.0.0.1:9000/api/",
+      }).then((res) => {
+        console.log(res);
+      });
+    },
+  },
+};
+</script>
+<style>
+#page-box {
+  width: 300px;
+  height: 600px;
+  box-shadow: 0 2px 12px 0 #000;
+  color: red;
+  margin: 0 auto;
+}
+#page-box img {
+  width: 100%;
+}
+</style>
